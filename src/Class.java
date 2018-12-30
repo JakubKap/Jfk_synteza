5b1c64b7048dda545599a8ac0a8a6630c46e4eb3
Jakub Kapusta <jakubkapusta39@gmail.com>
2018-12-30 21:14:42 +0100

    Temporart comment on asterisk and non-asterisk imports comparison

--- src/Class.java
+++ src/Class.java
@@ -7,31 +7,16 @@ import static java.lang.String.*;
 import static java.lang.System.*;
 import java.util.ArrayList;
 import java.io.FileInputStream;
-import javax.swing.*;
-import java.awt.Font.*;
-import java.awt.Stroke.*;
-import javax.swing.UIDefaults;
-import java.awt.TextArea;
-import java.awt.*;
-import java.awt.color.*;
-import java.awt.Stroke;
-import javax.swing.border.AbstractBorder;
-import java.awt.TextArea.*;
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+
+import java.awt.Color;
+import java.awt.*;
+
+import com.github.javaparser.TokenRange;
 
 
 public final class Class {
